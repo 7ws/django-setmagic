8dc7b1b963c2f7fb2b9df6cbdee8bc425aa4fc3c
Evan Myller <eMyller@7ws.co>
2014-06-30 21:22:19 -0300

    Starting the app code

---
+++ setmagic/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Group(models.Model):
+    label = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.label
+
+
+class Setting(models.Model):
+    group = models.ForeignKey('Group')
+    name = models.CharField(max_length=20, unique=True)
+    label = models.CharField(max_length=40)
+    help_text = models.CharField(max_length=140)
+
+    def __str__(self):
+        return '{name} ({label} - {group})'.format(
+            name=self.name,
+            label=self.label,
+            group=self.group.label,
+        )
