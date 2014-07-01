887856761202dbdaf0a279d80e01680ec180ce7a
Evan Myller <eMyller@7ws.co>
2014-07-01 14:28:23 -0300

    Make settings editable through admin

---
+++ setmagic/admin.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib import admin
+
+from setmagic.models import Setting
+
+
+_denied = lambda *args: False
+
+
+class SetMagicAdmin(admin.ModelAdmin):
+    list_display = 'label', 'current_value',
+    list_editable = 'current_value',
+    list_display_links = None
+
+    has_add_permission = _denied
+    has_delete_permission = _denied
+
+    # Make all fields read-only at the change form
+    def get_readonly_fields(self, *args, **kwargs):
+        return self.opts.get_all_field_names()
+
+admin.site.register(Setting, SetMagicAdmin)
