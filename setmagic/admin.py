d8cd319b24e81975ff480e0f64ee4f2d6a32e6d9
Evan Myller <eMyller@7ws.co>
2014-07-01 23:57:03 -0300

    Sync settings at the admin page load time

--- setmagic/admin.py
+++ setmagic/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from setmagic import settings
 from setmagic.models import Setting
 
 
@@ -17,5 +18,9 @@ class SetMagicAdmin(admin.ModelAdmin):
     # Make all fields read-only at the change form
     def get_readonly_fields(self, *args, **kwargs):
         return self.opts.get_all_field_names()
+
+    def changelist_view(self, *args, **kwargs):
+        settings._initialize()
+        return super(SetMagicAdmin, self).changelist_view(*args, **kwargs)
 
 admin.site.register(Setting, SetMagicAdmin)
