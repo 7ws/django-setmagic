5a6f6ec28efd410ee6400a729e380d3ce7f121e6
Evan Myller <eMyller@7ws.co>
2014-07-11 08:26:50 -0300

    Alter some settings field params for more flexibility

--- setmagic/models.py
+++ setmagic/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Setting(models.Model):
-    name = models.CharField(max_length=20, unique=True)
-    label = models.CharField(max_length=40)
-    help_text = models.CharField(max_length=140)
+    name = models.CharField(max_length=40, unique=True)
+    label = models.CharField(max_length=60)
+    help_text = models.TextField()
     current_value = models.CharField(max_length=100)
 
     class Meta:
