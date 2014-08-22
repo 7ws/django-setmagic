d2c552b8996ce1ef8a2d5ef64f6a2b60ce306cf3
Evan Myller <eMyller@7ws.co>
2014-08-22 06:54:03 -0300

    Add model unicode support for Python 2

--- setmagic/models.py
+++ setmagic/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Setting(models.Model):
     name = models.CharField(max_length=40, unique=True)
     label = models.CharField(max_length=60)
